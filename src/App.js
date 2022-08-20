79402df66f8e47a30c7fc6f48db327cbcafbd932
AL-Shahriar <anik7744@gmail.com>
2022-08-20 23:50:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import card from './style.css'
 function App() {
+  const [items, setItems] = useState([]);
+  const [noMore,setNomore] =useState(true);
+  const [page,setpage] = useState(2);
+  useEffect(() => {
+    const getProduct = async () => {
+      const res = await fetch(
+        "https://dummyjson.com/products?limit=10&page=1"
+      );
+      const data = await res.json();
+      setItems(data.products);
+      console.log('data',data);
+    };
+    getProduct();
+  }, []);
+
+  const getProductNext = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&page=${page}`);
+    const data = await res.json();
+    setItems(data.products);
+    console.log('data2',data);
+  };
+
+  const fetchData =() => {
+    const productss = getProductNext();
+    setItems([{...items, ...productss}]); 
+    if(productss.length ===0 || productss.length<10){
+    setNomore(false);
+    }
+    // setNomore(false);
+    setpage(page+1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="card">
+      <InfiniteScroll
+        dataLength={items?.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={noMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <h1>hello</h1>
+        {items.map((item) => {
+          return (
+          <div class="card">
+             <div class="container">
+            <h4>Name:{item.title}</h4> 
+            <p>Description:{item.description}</p> 
+          </div>
+        </div>
+        )
+        })}
+      </InfiniteScroll>
+      </div>
+
   );
 }
 
